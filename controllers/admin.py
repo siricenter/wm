f8e58a3f1c070103f41c1ce91bf545bc401bc9c5
mathewy <yan10001@byui.edu>
2014-07-01 00:53:52 -0600

    Added custom welcome message to admin page.

--- controllers/admin.py
+++ controllers/admin.py
@@ -4,6 +4,10 @@ def user(): return dict(form=auth())
 def download(): return response.download(request,db)
 def call(): return service()
 ### end requires
+
+@auth.requires_login()
 def index():
-	return dict()
+	username = auth.user.first_name;
+	
+	return locals()
 	
