e31fa469b67fa3442756fb975d3a74cff5a79625
mathewy <yan10001@byui.edu>
2014-07-08 10:53:14 -0600

    Removed manageUsers().

--- models/admin_views.py
+++ models/admin_views.py
@@ -1,7 +1,7 @@
-def manageUsers():
-	query = db.auth_user
-	grid = SQLFORM.grid(query=query)
-	return locals()
+import Admin
+def displayQuery():
+	myAdmin = AdminStuff(4,5)
+	return dict(myAdmin = myAdmin)
 	
 def test(a):
 	return a*a
