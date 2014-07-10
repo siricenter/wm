a8f91747a13d651c1455bd2b50c406acb44161d6
mathewy <yan10001@byui.edu>
2014-07-10 12:28:22 -0600

    Got it.

--- controllers/admin.py
+++ controllers/admin.py
@@ -11,30 +11,17 @@ from gluon.serializers import json
 
 @auth.requires_login()
 def index():
-	username = auth.user.first_name;
-	list = db.tables
-	tables = []
-	for x in range(len(list)):
-		match = re.search('archive',list[x])
-		if not match:
-			list[x] = list[x].replace('auth_','')
-			list[x] = re.sub('^t_', '', list[x])
-			list[x] = list[x].replace('_',' ')
-			tables.append(list[x].title())
-			continue
+	username = auth.user.first_name
+	tables = test()
 	return locals()
 
 @auth.requires_login()	
 def manageUsers():
-	query = db.auth_user
+	query = db.t_building
 	grid = SQLFORM.grid(query=query)
 	return grid
 
 def test():
-    return request.vars.tester
-
-import re
-def tables():
 	list = db.tables
 	tables = []
 	for x in range(len(list)):
@@ -43,9 +30,25 @@ def tables():
 			list[x] = list[x].replace('auth_','')
 			list[x] = re.sub('^t_', '', list[x])
 			list[x] = list[x].replace('_',' ')
-			tables.append(list[x].title())
+			if list[x] not in ('event','cas'):
+				tables.append(list[x].title())
 			continue
-	return response.json(tables)
+		continue
+	return tables
+
+import re
+def tables():
+	testList = db.tables
+	test = []
+	for x in range(len(testList)):
+		match = re.search('archive',testList[x])
+		if not match:
+			val = testList[x].replace('auth_','')
+			val = re.sub('^t_', '', val)
+			val = val.replace('_',' ')
+			test.append({testList[x]:val.title()})
+			continue
+	return response.json(test)
 	
 def testing():
 	list = db.tables
